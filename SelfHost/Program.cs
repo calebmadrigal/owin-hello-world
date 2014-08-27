7ec8945ecd8c1490e555226b2111d7a8322b86a8
Caleb Madrigal <caleb.madrigal@gmail.com>
2014-08-27 11:25:27 -0400

    Added project.

---
+++ SelfHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:1337";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("Listening on " + url);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SelfHost/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SelfHost.Startup))]
+
+namespace SelfHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(ctx =>
+                {
+                    IOwinRequest req = ctx.Request;
+                    IOwinResponse res = ctx.Response;
+                    var name = req.Query["name"];
+                    if (name == null)
+                    {
+                        return res.WriteAsync("Hello World");
+                    }
+                    else
+                    {
+                        return res.WriteAsync("Hello " + name);
+                    }
+                });
+        }
+    }
+}
